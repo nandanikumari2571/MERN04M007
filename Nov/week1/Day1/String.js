dececf314e37ec2be7d832c161eeff75c355810a
nandanikumari2571 <kumarinandani2571@gmail.com>
2025-11-03 19:31:03 +0530

    Update

---
+++ Nov/week1/Day1/String.js
@@ -0,0 +1,67 @@
+console.log("Welcome to Javscript String World")
+
+//Let str1='first string';
+//et str2='second string';
+//Let str3='third string';
+//console.log(typeof str1)
+//console.log(str2)
+//console.log(str3)
+
+
+
+
+
+// let str="Hello"
+// console.log(str[0])
+
+// let obj1={
+//     name:"tunguru"
+// }
+
+// console.log(obj1.name)
+// obj1.name="tunguru"
+// console.log(obj1.name);
+
+
+let str="2Hello"
+console.log(str[0])
+
+str[0]="S";
+console.log(typeof str[0]);
+
+// variable
+// object
+console.log(str)
+
+
+let strobj=new String("Bye");
+console.log(strobj.valueOf()[3])
+
+str=new String("New String");
+console.log(str)
+
+// string variable
+// string methods
+// property :
+
+console.log(str.length)
+
+
+
+
+// String methods
+// toUpperCase()
+
+
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+
+let nameStr="brand bacardi"
+console.log(nameStr+"limon")
+
+
+console.log(nameStr,30)
+// let ml=50;
+console.log('brand bacardi in ${ml}ml')
+
