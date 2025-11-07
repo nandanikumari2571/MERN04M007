e1c19f4ace4cfb008f7497a775e5c43b9a2c20eb
nandanikumari2571 <kumarinandani2571@gmail.com>
2025-11-07 19:29:25 +0530

    Update

---
+++ Nov/week1/Day3practice/string.js
@@ -0,0 +1,70 @@
+// Q.1 Write a program to count the number of vowels in a string.
+// const inputString = "String";
+// console.log(`Number of vowels: ${countVowels(inputString)}`);
+
+
+
+// Q.11 Splite the sentence into an array pf words and print the longest word.
+
+let sentence = "JavaScript is a powerful programming language";
+let words = sentence.split(" ");
+
+let longestWord = words[0];
+
+console.log("The longest word is:", longestWord);
+
+
+// Q.12 convert the string like"hello_world" into Hello world.
+
+let str = "hello_world";
+let result = str.replace("_", " ");
+result = result.charAt(0).toUpperCase() + result.slice(1);
+console.log(result); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let str1 = "hello_world";
+let result1 = str.replace("_"," ");
+result = result.chartAt(0).toUpperCase() + result.slice(1);
+console.log(result);
