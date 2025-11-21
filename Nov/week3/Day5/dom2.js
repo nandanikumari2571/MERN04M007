123381115392d8a6f9437c1db592e9d3cb79abd1
nandanikumari2571 <kumarinandani2571@gmail.com>
2025-11-21 19:27:16 +0530

    Update

---
+++ Nov/week3/Day5/dom2.js
@@ -0,0 +1,15 @@
+console.log("today we are going to learn DOM object")
+// querySelector();
+let h1 = document.querySelector("#head");
+console.log(h1);
+
+let divFirst = document.querySelector(".box");
+console.log(divFirst);
+
+let Firstp= document.querySelector("p");
+console.log(Firstp);
+
+// querySelectorAll();
+let classes = document.querySelectorAll(".box");
+console.log(classes);
+
